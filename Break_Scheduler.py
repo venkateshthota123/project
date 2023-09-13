f44090ed6f585b4f25dde1251043e05a784e1b5a
venkateshthota123 <115466027+venkateshthota123@users.noreply.github.com>
2023-09-13 10:52:19 +0530

    Create Break_Scheduler.py

---
+++ Break_Scheduler.py
@@ -0,0 +1,15 @@
+import time
+import webbrowser
+type_break=input("enter the dtype of break M for minutes H for hours")
+breaks=int(input("enter the no of breaks"))
+t=int(input("enter no of minutes or hours after the break"))
+z=input("enter the youtube link")
+
+for i in range(breaks):
+
+    if type_break.lower()=='m':
+        time.sleep(t)
+        webbrowser.open(z,new=2)
+    else:
+        time.sleep(t)
+        webbrowser.open(z,new=2)
