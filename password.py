1389ba08b9cb84933242cd7dd125cf0f7d34dae0
venkateshthota123 <115466027+venkateshthota123@users.noreply.github.com>
2024-02-19 23:56:53 +0530

    password.py

--- password.py
+++ password.py
@@ -1,24 +1,32 @@
-import random
-import string
-def random_password(k):
-    """
-    pass the length of the value to module
-    
-    create a random password with required length generator with 1 capital,1 numerical
-    1 special charcter and remaning all small charcters 
-        
-    it required only to pass required length only
-        
-    """
+import random 
+import string 
+from tkinter import *
+from tkinter import messagebox
+import pyperclip
+
+
+
+def password():
+    length=int(e1.get())
     upper=string.ascii_uppercase
     lower=string.ascii_lowercase
     digits=string.digits
-    special_charcter="@_-"
-    password="".join(random.choice(upper)+random.choice(digits)+random.choice(special_charcter))
-    password+="".join(random.choices(lower,k=k-3))
+    special_charcter="~!@#$%^&*()_+{}:\"<>?`-=[];\',./=\\"
+    password=upper+lower+digits+special_charcter
+    password="".join(random.choices(password,k=length))
     password=list(password)
     random.shuffle(password)
-    return "".join(password)
-k=int(input("enter the length of password:"))
-print(f"The Random password is {random_password(k)}")
+    password_gen="".join(password)
+    messagebox.showinfo("Result",f'your random password : {password_gen}')
+    pyperclip.copy(password_gen)
 
+
+
+base=Tk()
+base.geometry("250x200")
+base.title("Password generator")
+e1=StringVar()
+Label(base,text= "Enter the length of password").pack()
+Entry(base,textvariable = e1).pack()
+Button(base, text="Generate Password",command=password,height=1,width=15).pack()
+base.mainloop()
